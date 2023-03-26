cc89081ba156e656f67925ec24e7139f7b4718ee
github_username <you@domain.com>
2023-03-27 03:18:59 +0600

    complete

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -2,15 +2,24 @@ import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css'
 
+
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
 // console.log(products)
+const [cart, setCart] = useState([])
     useEffect(()=>{
-        fetch('/src/fakeData/products.json')
+        // fetch('/src/fakeData/products.json')
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
-    },[])
+    },[]);
+
+    const handleAddToCart = (product) => {
+        // console.log(produc)
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className='shop-container'>
@@ -20,11 +29,13 @@ const Shop = () => {
                     products.map(product => <Product
                     key={product.id}
                     product={product}
+                    handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h3>Order Summary</h3>
+                <p>Cart Item : {cart.length}</p>
             </div>
         </div>
     );
